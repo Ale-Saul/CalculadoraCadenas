9872b1168cd032f9ab1112c4a08b848eb7eed366
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-13 22:32:24 -0400

    Se agrego a la funcion cuando se le pasa un numero en cadena retorna ese numero

--- src/sumadorDeCadenas.js
+++ src/sumadorDeCadenas.js
@@ -2,6 +2,7 @@ function sumarCadena(cadena) {
     if (isNaN(cadena)) {
       return 0;
     }
+    return Number.parseInt(cadena);
 }
 
 export default sumarCadena;
--- src/sumadorDeCadenas.spec.js
+++ src/sumadorDeCadenas.spec.js
@@ -4,5 +4,8 @@ describe("Sumar Cadenas", () => {
     it("deberia retornar 0 si la cadena que se le pasa esta vacia", () => {
       expect(sumarCadena(NaN)).toEqual(0);
     });
+    it("deberia retornar 2 si la cadena de entrada es 2", () =>{
+        expect(sumarCadena("2")).toEqual(2);
+    })
   });
   
