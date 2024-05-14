31d4fe31401399a29a9c5be577a88fca72f49478
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-13 22:50:37 -0400

    Se agrego a la funcion que sume dos numeros dentro de la cadena

--- src/sumadorDeCadenas.js
+++ src/sumadorDeCadenas.js
@@ -1,8 +1,20 @@
 function sumarCadena(cadena) {
-    if (isNaN(cadena)) {
-      return 0;
-    }
+    if(cadena === "") 
+        return 0;
+    if(cadena.includes(","))
+        return sumar(cadena);
+    
     return Number.parseInt(cadena);
+
+    function sumar(cadena){
+        const numeros = cadena.split(",");
+        let suma = 0;
+        for (let i = 0; i < numeros.length; i++) {
+          console.log(numeros[i]);  
+          suma += Number.parseInt(numeros[i]);
+        }
+        return suma;
+    }
 }
 
 export default sumarCadena;
--- src/sumadorDeCadenas.spec.js
+++ src/sumadorDeCadenas.spec.js
@@ -2,10 +2,13 @@ import sumarCadena from './sumadorDeCadenas';
 
 describe("Sumar Cadenas", () => {
     it("deberia retornar 0 si la cadena que se le pasa esta vacia", () => {
-      expect(sumarCadena(NaN)).toEqual(0);
+      expect(sumarCadena("")).toEqual(0);
     });
     it("deberia retornar 2 si la cadena de entrada es 2", () =>{
         expect(sumarCadena("2")).toEqual(2);
+    })
+    it("deberia retornar 3 si la cadena de entrada es 1,2", () =>{
+        expect(sumarCadena("1,2")).toEqual(3);
     })
   });
   
