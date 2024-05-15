047d0bccf82cd70003f0b8172f175164930a9735
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-15 18:13:16 -0400

    Se cambio la funcion sumarCadena para que sea mas corta

--- src/sumadorDeCadenas.js
+++ src/sumadorDeCadenas.js
@@ -6,31 +6,28 @@ function sumarCadena(cadena) {
     if(cadena.includes(",") || cadena.includes("-"))
         return sumar(cadena);
 
-    
     return Number.parseInt(cadena);
-
-    function sumar(cadena){
-        const numeros = cadena.split(/,|-/);
-        let suma = 0;
-        for (let i = 0; i < numeros.length; i++) {
-            const numero = Number.parseInt(numeros[i]);
-            if(numero < 1000)
-                suma += Number.parseInt(numeros[i]);
-        }
-        return suma;
+}
+function sumar(cadena){
+    const numeros = cadena.split(/,|-/);
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        const numero = Number.parseInt(numeros[i]);
+        if(numero < 1000)
+            suma += Number.parseInt(numeros[i]);
     }
-
-    function sumarPersonalizado(cadena){
-        const delimitador = cadena.substring(3, 4);
-        const numeros = cadena.substring(6).split(new RegExp(`,|-|${delimitador}`));
-        let suma = 0;
-        for (let i = 0; i < numeros.length; i++) {
-            const numero = Number.parseInt(numeros[i]);
-            if(numero < 1000)
-                suma += Number.parseInt(numeros[i]);
-        }
-        return suma;
-    }
+    return suma;
 }
 
+function sumarPersonalizado(cadena){
+    const delimitador = cadena.substring(3, 4);
+    const numeros = cadena.substring(6).split(new RegExp(`,|-|${delimitador}`));
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        const numero = Number.parseInt(numeros[i]);
+        if(numero < 1000)
+            suma += Number.parseInt(numeros[i]);
+    }
+    return suma;
+}
 export default sumarCadena;
