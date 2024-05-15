f367959523c0954ce5c567c3a9e688197392f2dc
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-15 18:23:52 -0400

    Se cambio el nombre a cadenaInput en el presenter

--- src/presenter.js
+++ src/presenter.js
@@ -7,7 +7,7 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const cadenaInput = cadenaEntrada.value;
+  const cadena = cadenaEntrada.value;
 
-  div.innerHTML = "<p>" + sumarCadena(cadenaInput) + "</p>";
+  div.innerHTML = "<p>" + sumarCadena(cadena) + "</p>";
 });
