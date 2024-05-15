92469126b2ac4ae65a9d62d89fdd38e181893b13
Alejandro <alejandro.mont.cesp@gmail.com>
2024-05-15 18:19:26 -0400

    Se cambio el nombre a firstChar en el presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,13 @@
 import sumarCadena from "./sumadorDeCadenas";
 
-const firstChar = document.querySelector("#cadena");
+const cadenaEntrada = document.querySelector("#cadena");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const cadenaInput = firstChar.value;
+  const cadenaInput = cadenaEntrada.value;
 
   div.innerHTML = "<p>" + sumarCadena(cadenaInput) + "</p>";
 });
